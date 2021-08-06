46b17c241b678aec682f8b8beb64b8948ab88b39
elizahuang <eliza85827@gmail.com>
2021-08-07 03:37:45 +0800

    add try catch to proactive msg api

--- azure_chatbot/app.py
+++ azure_chatbot/app.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import sys,os,json,requests,copy
+import sys,os,json,requests,copy,logging
 import traceback
 from datetime import datetime
 
@@ -80,78 +80,84 @@ async def messages(req: Request) -> Response:
 routes = web.RouteTableDef()
 @routes.post('/api/v1/cron-messages')
 async def sendReminder(request):
-    todoInfo = await request.json()  # suppose a dict now    
-    # {'tenant_id': '98fb227d-9ccb-46af-bbc7-7adfb3090fa6', 
-    # 'user_id': '29:1EHxt_msQczs0jFpTh3HUdDzzbQhOQLsins3m8VHWjctZ2DF7Htq-89Dmv3T-t6KhnJxn2uzoNL1r5qNEu3myiQ', 
-    # 'todo':  {'todo_id': 'Gq31ES4ipg', 'todo_name': 'test_cronjob', 
-    # 'todo_date': datetime.datetime(2021, 8, 6, 2, 0), 
-    # 'todo_contents': 'test_cronjob', 'todo_update_date': '2021/08/05', 
-    # 'todo_completed': False, 'employee_id': '109491'}}
-    teams_appid='30eba4f2-6e15-458b-9fdf-f8bbf25efb4f'
-    appSecret='ElizaHuangTaigidian2021'
-    botId='28:30eba4f2-6e15-458b-9fdf-f8bbf25efb4f'
-    userId=todoInfo['user_id']    
-    tenant_id=todoInfo['tenant_id']
-    cardToSend=prepareReminderCard(todoInfo["todo"])
-    ## access token
-    url='https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token'
-    payload = {'Host': 'login.microsoftonline.com',
-        "Content-Type": "application/x-www-form-urlencoded",
-        'grant_type':'client_credentials',
-        'client_id':teams_appid,
-        'client_secret':appSecret,
-        'scope':'https://api.botframework.com/.default'}
-    
-    r=requests.post(url, data=(payload))
-    response=json.loads(r.content.decode('utf-8'))  
-    access_token=response['access_token']  
-    print('access_token:\n',access_token) 
-    
-    ## get conversationId
-    header={'Authorization': 'Bearer ' + access_token} #, 'content-type':'application/json'
-    url=f'https://smba.trafficmanager.net/apac/v3/conversations'
-    payload={
-        "bot": {
-            "id": botId,#30eba4f2-6e15-458b-9fdf-f8bbf25efb4f
-            # "name": "AzureBot001_Regis"
-        },
-        "isGroup": False,
-        "members": [
-            {
-                "id": userId, #usesrid_office,
-                "name":"" #"Yi Huang 黃懿"
-            }
-        ],
-        "tenantId": tenant_id,
-        "topicName": "proactive msg"
-    }
-    conversation_response=json.loads(requests.post(url,json=(payload), headers=header).content.decode('utf-8'))
-    conversation_id=conversation_response["id"]
-    print('conversation_id: \n',conversation_id)
+    try:
+        todoInfo = await request.json()  # suppose a dict now    
+        # {'tenant_id': '98fb227d-9ccb-46af-bbc7-7adfb3090fa6', 
+        # 'user_id': '29:1EHxt_msQczs0jFpTh3HUdDzzbQhOQLsins3m8VHWjctZ2DF7Htq-89Dmv3T-t6KhnJxn2uzoNL1r5qNEu3myiQ', 
+        # 'todo':  {'todo_id': 'Gq31ES4ipg', 'todo_name': 'test_cronjob', 
+        # 'todo_date': datetime.datetime(2021, 8, 6, 2, 0), 
+        # 'todo_contents': 'test_cronjob', 'todo_update_date': '2021/08/05', 
+        # 'todo_completed': False, 'employee_id': '109491'}}
+        teams_appid='30eba4f2-6e15-458b-9fdf-f8bbf25efb4f'
+        appSecret='ElizaHuangTaigidian2021'
+        botId='28:30eba4f2-6e15-458b-9fdf-f8bbf25efb4f'
+        userId=todoInfo['user_id']    
+        tenant_id=todoInfo['tenant_id']
+        cardToSend=prepareReminderCard(todoInfo["todo"])
+        ## access token
+        url='https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token'
+        payload = {'Host': 'login.microsoftonline.com',
+            "Content-Type": "application/x-www-form-urlencoded",
+            'grant_type':'client_credentials',
+            'client_id':teams_appid,
+            'client_secret':appSecret,
+            'scope':'https://api.botframework.com/.default'}
+        
+        r=requests.post(url, data=(payload))
+        response=json.loads(r.content.decode('utf-8'))  
+        access_token=response['access_token']  
+        print('access_token:\n',access_token) 
+        
+        ## get conversationId
+        header={'Authorization': 'Bearer ' + access_token} #, 'content-type':'application/json'
+        url=f'https://smba.trafficmanager.net/apac/v3/conversations'
+        payload={
+            "bot": {
+                "id": botId,#30eba4f2-6e15-458b-9fdf-f8bbf25efb4f
+                # "name": "AzureBot001_Regis"
+            },
+            "isGroup": False,
+            "members": [
+                {
+                    "id": userId, #usesrid_office,
+                    "name":"" #"Yi Huang 黃懿"
+                }
+            ],
+            "tenantId": tenant_id,
+            "topicName": "proactive msg"
+        }
+        conversation_response=json.loads(requests.post(url,json=(payload), headers=header).content.decode('utf-8'))
+        conversation_id=conversation_response["id"]
+        print('conversation_id: \n',conversation_id)
 
-    url=f'https://smba.trafficmanager.net/apac/v3/conversations/%s/activities'%(conversation_id)
-    payload_template={
-        "type": "message",
-        "from": {
-            "id": botId,
-            "name": "AzureBot001_Regis"
-        },
-        "conversation": {
-            "id": conversation_id,
-            "name": "send proactive msg now"
-        },
-        "recipient": {
-            "id": userId,
-            "name":"",# "Yi Huang 黃懿"
-        },
-        "attachments": []
-    }
-    payload=copy.deepcopy(payload_template)
-    payload["attachments"]+=[cardToSend]
-    response_forSendMsg =requests.post(url, json=(payload), headers=header).content.decode('utf-8')
-    print('response_forSendMsg',response_forSendMsg)
-
-    return
+        url=f'https://smba.trafficmanager.net/apac/v3/conversations/%s/activities'%(conversation_id)
+        payload_template={
+            "type": "message",
+            "from": {
+                "id": botId,
+                "name": "AzureBot001_Regis"
+            },
+            "conversation": {
+                "id": conversation_id,
+                "name": "send proactive msg now"
+            },
+            "recipient": {
+                "id": userId,
+                "name":"",# "Yi Huang 黃懿"
+            },
+            "attachments": []
+        }
+        payload=copy.deepcopy(payload_template)
+        payload["attachments"]+=[cardToSend]
+        response_forSendMsg =requests.post(url, json=(payload), headers=header).content.decode('utf-8')
+        print('response_forSendMsg',response_forSendMsg)
+        
+        return Response(status=200)
+    
+    except Exception as e:
+        logging.error('Error Msg: ',  exc_info=e)
+        return Response(status=500)
+    
 
 
 APP = web.Application(middlewares=[aiohttp_error_middleware])
