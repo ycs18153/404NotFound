5ce2b9b26827172df1bdbb574c00e4b4a3977367
Andy <37218627+ycs18153@users.noreply.github.com>
2021-08-08 19:49:42 +0800

    Add files via upload

--- line_chatbot/magicMessage/carousel.py
+++ line_chatbot/magicMessage/carousel.py
@@ -1,289 +1,159 @@
 todo = {
-    "type": "bubble",
-    "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-            {
-                "type": "text",
-                "text": "Your Todo List",
-                "weight": "bold",
-                "size": "xxl",
-                "margin": "md"
-            },
-            {
-                "type": "separator",
-                "margin": "xxl"
-            },
-            {
-                "type": "box",
-                "layout": "vertical",
-                "margin": "xxl",
-                "spacing": "sm",
-                "contents": [
-                    # insert six {new_info} here
-                    # insert one {update_btn here}
-
-                ]
-            },
-            {
-                "type": "separator",
-                "margin": "xxl"
-            },
-            {
-                "type": "box",
-                "layout": "horizontal",
-                "margin": "md",
-                "contents": [
-                    {
-                        "type": "button",
-                        "action": {
-                            "type": "uri",
-                            "label": "create",
-                            "uri": "http://linecorp.com/"
-                        },
-                        "style": "primary"
-                    },
-                    {
-                        "type": "button",
-                        "action": {
-                            "type": "uri",
-                            "label": "delete",
-                            "uri": "http://linecorp.com/"
-                        },
-                        "style": "primary",
-                        "margin": "10px"
-                    }
-                ]
-            }
-        ]
-    },
-    "styles": {
-        "footer": {
-            "separator": True
-        }
-    }
-}
-
-new_info = {
-    "type": "box",
-    "layout": "horizontal",
+    "type": "carousel",
     "contents": [
-        {
-            "type": "text",
-            "text": "Test",
-            "size": "sm",
-            "color": "#555555",
-            "flex": 0
-        },
-        {
-            "type": "text",
-            "text": "QQQ",
-            "size": "sm",
-            "color": "#111111",
-            "align": "end"
-        }
+
     ]
 }
 
-update_btn = {
-    "type": "button",
-    "action": {
-        "type": "message",
-        "label": "update",
-        "text": "update"
-    },
-    "height": "sm",
-    "style": "secondary"
-}
-
-original = {
+new_info = {
     "type": "bubble",
+    "size": "kilo",
+    "header": {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+            {
+                "type": "text",
+                "text": "Todo ID:",
+                "color": "#ffffff",
+                "align": "start",
+                "size": "md",
+                "gravity": "center"
+            },
+            {
+                "type": "text",
+                "text": "",
+                "align": "start",
+            }
+        ],
+        "backgroundColor": "#27ACB2",
+        "paddingTop": "19px",
+        "paddingAll": "12px",
+        "paddingBottom": "16px"
+    },
     "body": {
         "type": "box",
         "layout": "vertical",
         "contents": [
             {
-                "type": "text",
-                "text": "Brown Store",
-                "weight": "bold",
-                "size": "xxl",
-                "margin": "md"
-            },
-            {
-                "type": "separator",
-                "margin": "xxl"
-            },
-            {
                 "type": "box",
-                "layout": "vertical",
-                "margin": "xxl",
-                "spacing": "sm",
-                "conten ts": [
+                "layout": "horizontal",
+                "contents": [
                     {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                            {
-                                "type": "text",
-                                "text": "Todo ID",
-                                "size": "sm",
-                                "color": "#555555",
-                                "flex": 0
-                            },
-                            {
-                                "type": "text",
-                                "text": "$2.99",
-                                "size": "sm",
-                                "color": "#111111",
-                                "align": "end"
-                            }
-                        ]
+                        "type": "text",
+                        "text": "Title"
                     },
                     {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                            {
-                                "type": "text",
-                                "text": "Todo Title",
-                                "size": "sm",
-                                "color": "#555555",
-                                "flex": 0
-                            },
-                            {
-                                "type": "text",
-                                "text": "$0.99",
-                                "size": "sm",
-                                "color": "#111111",
-                                "align": "end"
-                            }
-                        ]
-                    },
-                    {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                            {
-                                "type": "text",
-                                "text": "Todo Date",
-                                "size": "sm",
-                                "color": "#555555",
-                                "flex": 0
-                            },
-                            {
-                                "type": "text",
-                                "text": "$3.33",
-                                "size": "sm",
-                                "color": "#111111",
-                                "align": "end"
-                            }
-                        ]
-                    },
-                    {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                            {
-                                "type": "text",
-                                "text": "Description",
-                                "size": "sm",
-                                "color": "#555555"
-                            },
-                            {
-                                "type": "text",
-                                "text": "$7.31",
-                                "size": "sm",
-                                "color": "#111111",
-                                "align": "end"
-                            }
-                        ]
-                    },
-                    {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                            {
-                                "type": "text",
-                                "text": "Update Date",
-                                "size": "sm",
-                                "color": "#555555"
-                            },
-                            {
-                                "type": "text",
-                                "text": "$8.0",
-                                "size": "sm",
-                                "color": "#111111",
-                                "align": "end"
-                            }
-                        ]
-                    },
-                    {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                            {
-                                "type": "text",
-                                "text": "Todo Status",
-                                "size": "sm",
-                                "color": "#555555"
-                            },
-                            {
-                                "type": "text",
-                                "text": "$0.69",
-                                "size": "sm",
-                                "color": "#111111",
-                                "align": "end"
-                            }
-                        ]
-                    },
-                    {
-                        "type": "button",
-                        "action": {
-                            "type": "message",
-                            "label": "update",
-                            "text": "update"
-                        },
-                        "height": "sm",
-                        "style": "secondary"
+                        "type": "text",
+                        "text": "",
+                        "color": "#8C8C8C",
+                        "size": "sm",
+                        "align": "end",
+                        "wrap": True
                     }
                 ]
-            },
-            {
-                "type": "separator",
-                "margin": "xxl"
             },
             {
                 "type": "box",
                 "layout": "horizontal",
-                "margin": "md",
                 "contents": [
                     {
-                        "type": "button",
-                        "action": {
-                            "type": "uri",
-                            "label": "create",
-                            "uri": "http://linecorp.com/"
-                        },
-                        "style": "primary"
+                        "type": "text",
+                        "text": "When"
                     },
                     {
-                        "type": "button",
-                        "action": {
-                            "type": "uri",
-                            "label": "delete",
-                            "uri": "http://linecorp.com/"
-                        },
-                        "style": "primary",
-                        "margin": "10px"
+                        "type": "text",
+                        "text": "",
+                        "size": "sm",
+                        "align": "end",
+                        "color": "#8C8C8C",
+                        "wrap": True
                     }
                 ]
+            },
+            {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "Description"
+                    },
+                    {
+                        "type": "text",
+                        "text": "",
+                        "size": "sm",
+                        "align": "end",
+                        "color": "#8C8C8C",
+                        "wrap": True
+                    }
+                ]
+            },
+            {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "Create Date"
+                    },
+                    {
+                        "type": "text",
+                        "text": "",
+                        "size": "sm",
+                        "align": "end",
+                        "color": "#8C8C8C"
+                    }
+                ]
+            },
+            {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "Status"
+                    },
+                    {
+                        "type": "text",
+                        "text": "",
+                        "size": "sm",
+                        "align": "end",
+                        "color": "#8C8C8C"
+                    }
+                ]
+            }
+        ],
+        "spacing": "md",
+        "paddingAll": "12px"
+    },
+    "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+                "type": "button",
+                "action": {
+                    "type": "uri",
+                    "label": "Update",
+                    "uri": "https://liff.line.me/1656294870-YzrJDAmr"
+                }
+            },
+            {
+                "type": "button",
+                "style": "link",
+                "height": "sm",
+                "action": {
+                    "type": "message",
+                    "label": "Delete",
+                    "text": ""
+                }
             }
         ]
     },
     "styles": {
         "footer": {
-            "separator": True
+            "separator": False
         }
     }
 }
